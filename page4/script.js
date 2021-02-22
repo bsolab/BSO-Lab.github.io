60168c83c3e630b5e2649e66ab83328e7b005d18
bsolab <79374777+bsolab@users.noreply.github.com>
2021-02-22 18:15:42 +0800

    Add files via upload

---
+++ page4/script.js
@@ -0,0 +1,347 @@
+document.addEventListener('DOMContentLoaded', function () {
+  var modeSwitch = document.querySelector('.mode-switch');
+
+  modeSwitch.addEventListener('click', function () {                     document.documentElement.classList.toggle('dark');
+    modeSwitch.classList.toggle('active');
+  });
+  
+  var listView = document.querySelector('.list-view');
+  var gridView = document.querySelector('.grid-view');
+  var projectsList = document.querySelector('.project-boxes');
+  
+ 
+  
+  gridView.addEventListener('click', function () {
+    gridView.classList.add('active');
+    listView.classList.remove('active');
+    projectsList.classList.remove('jsListView');
+    projectsList.classList.add('jsGridView');
+  });
+  
+  document.querySelector('.messages-btn').addEventListener('click', function () {
+    document.querySelector('.messages-section').classList.add('show');
+  });
+  
+  document.querySelector('.messages-close').addEventListener('click', function() {
+    document.querySelector('.messages-section').classList.remove('show');
+  });
+});
+
+var $messages = $('.messages-content'),
+    d, h, m,
+    i = 0;
+
+$(window).load(function() {
+  $messages.mCustomScrollbar();
+  setTimeout(function() {
+    fakeMessage();
+  }, 100);
+});
+
+function updateScrollbar() {
+  $messages.mCustomScrollbar("update").mCustomScrollbar('scrollTo', 'bottom', {
+    scrollInertia: 10,
+    timeout: 0
+  });
+}
+
+function setDate(){
+  d = new Date()
+  if (m != d.getMinutes()) {
+    m = d.getMinutes();
+    $('<div class="timestamp">' + d.getHours() + ':' + m + '</div>').appendTo($('.message:last'));
+  }
+}
+
+function insertMessage() {
+  msg = $('.message-input').val();
+  if ($.trim(msg) == '') {
+    return false;
+  }
+  $('<div class="message message-personal">' + msg + '</div>').appendTo($('.mCSB_container')).addClass('new');
+  setDate();
+  $('.message-input').val(null);
+  updateScrollbar();
+  setTimeout(function() {
+    fakeMessage();
+  }, 1000 + (Math.random() * 20) * 100);
+}
+
+$('.message-submit').click(function() {
+  insertMessage();
+});
+
+$(window).on('keydown', function(e) {
+  if (e.which == 13) {
+    insertMessage();
+    return false;
+  }
+})
+
+var Fake = [
+  '你知道台灣人平均只有一顆睪丸嗎',
+  '事情不是憨人想的那麼甘單',
+  '你有聽過”ATM“理論嗎?',
+  '想當年我在涼山特勤中心當醫官的時候．．．',
+  '我是想要白色的啦，但翰翰想要白色的~',
+  '你知道台灣人平均只有一顆睪丸嗎',
+  '事情不是憨人想的那麼甘單',
+  '你有聽過”ATM“理論嗎?',
+  '想當年我在涼山特勤中心當醫官的時候．．．',
+  '我是想要白色的啦，但翰翰想要白色的~',
+  '你知道台灣人平均只有一顆睪丸嗎',
+  '事情不是憨人想的那麼甘單',
+  '你有聽過”ATM“理論嗎?',
+  '想當年我在涼山特勤中心當醫官的時候．．．',
+  '我是想要白色的啦，但翰翰想要白色的~'
+]
+
+function fakeMessage() {
+  if ($('.message-input').val() != '') {
+    return false;
+  }
+  $('<div class="message loading new"><figure class="avatar"><img src="https://nctubsolab.weebly.com/uploads/1/3/1/6/131623033/834255909_orig.jpg" /></figure><span></span></div>').appendTo($('.mCSB_container'));
+  updateScrollbar();
+
+  setTimeout(function() {
+    $('.message.loading').remove();
+    $('<div class="message new"><figure class="avatar"><img src="https://nctubsolab.weebly.com/uploads/1/3/1/6/131623033/834255909_orig.jpg" /></figure>' + Fake[i] + '</div>').appendTo($('.mCSB_container')).addClass('new');
+    setDate();
+    updateScrollbar();
+    i++;
+  }, 1000 + (Math.random() * 20) * 100);
+
+}
+$(function()
+{
+    var playerTrack = $("#player-track"), bgArtwork = $('#bg-artwork'), bgArtworkUrl, albumName = $('#album-name'), trackName = $('#track-name'), albumArt = $('#album-art'), sArea = $('#s-area'), seekBar = $('#seek-bar'), trackTime = $('#track-time'), insTime = $('#ins-time'), sHover = $('#s-hover'), playPauseButton = $("#play-pause-button"),  i = playPauseButton.find('i'), tProgress = $('#current-time'), tTime = $('#track-length'), seekT, seekLoc, seekBarPos, cM, ctMinutes, ctSeconds, curMinutes, curSeconds, durMinutes, durSeconds, playProgress, bTime, nTime = 0, buffInterval = null, tFlag = false, albums = ['2015 交大生醫商機論壇 ”你累了嗎“'], trackNames = ['Professor - Chen'], albumArtworks = ['_1'], trackUrl = ['music1.mp3'], playPreviousTrackButton = $('#play-previous'), playNextTrackButton = $('#play-next'), currIndex = -1;
+
+    function playPause()
+    {
+        setTimeout(function()
+        {
+            if(audio.paused)
+            {
+                playerTrack.addClass('active');
+                albumArt.addClass('active');
+                checkBuffering();
+                i.attr('class','fas fa-pause');
+                audio.play();
+            }
+            else
+            {
+                playerTrack.removeClass('active');
+                albumArt.removeClass('active');
+                clearInterval(buffInterval);
+                albumArt.removeClass('buffering');
+                i.attr('class','fas fa-play');
+                audio.pause();
+            }
+        },300);
+    }
+
+      
+  function showHover(event)
+  {
+    seekBarPos = sArea.offset(); 
+    seekT = event.clientX - seekBarPos.left;
+    seekLoc = audio.duration * (seekT / sArea.outerWidth());
+    
+    sHover.width(seekT);
+    
+    cM = seekLoc / 60;
+    
+    ctMinutes = Math.floor(cM);
+    ctSeconds = Math.floor(seekLoc - ctMinutes * 60);
+    
+    if( (ctMinutes < 0) || (ctSeconds < 0) )
+      return;
+    
+        if( (ctMinutes < 0) || (ctSeconds < 0) )
+      return;
+    
+    if(ctMinutes < 10)
+      ctMinutes = '0'+ctMinutes;
+    if(ctSeconds < 10)
+      ctSeconds = '0'+ctSeconds;
+        
+        if( isNaN(ctMinutes) || isNaN(ctSeconds) )
+            insTime.text('--:--');
+        else
+        insTime.text(ctMinutes+':'+ctSeconds);
+            
+    insTime.css({'left':seekT,'margin-left':'-21px'}).fadeIn(0);
+    
+  }
+
+    function hideHover()
+  {
+        sHover.width(0);
+        insTime.text('00:00').css({'left':'0px','margin-left':'0px'}).fadeOut(0);   
+    }
+    
+    function playFromClickedPos()
+    {
+        audio.currentTime = seekLoc;
+    seekBar.width(seekT);
+    hideHover();
+    }
+
+    function updateCurrTime()
+  {
+        nTime = new Date();
+        nTime = nTime.getTime();
+
+        if( !tFlag )
+        {
+            tFlag = true;
+            trackTime.addClass('active');
+        }
+
+    curMinutes = Math.floor(audio.currentTime / 60);
+    curSeconds = Math.floor(audio.currentTime - curMinutes * 60);
+    
+    durMinutes = Math.floor(audio.duration / 60);
+    durSeconds = Math.floor(audio.duration - durMinutes * 60);
+    
+    playProgress = (audio.currentTime / audio.duration) * 100;
+    
+    if(curMinutes < 10)
+      curMinutes = '0'+curMinutes;
+    if(curSeconds < 10)
+      curSeconds = '0'+curSeconds;
+    
+    if(durMinutes < 10)
+      durMinutes = '0'+durMinutes;
+    if(durSeconds < 10)
+      durSeconds = '0'+durSeconds;
+        
+        if( isNaN(curMinutes) || isNaN(curSeconds) )
+            tProgress.text('00:00');
+        else
+        tProgress.text(curMinutes+':'+curSeconds);
+        
+        if( isNaN(durMinutes) || isNaN(durSeconds) )
+            tTime.text('00:00');
+        else
+        tTime.text(durMinutes+':'+durSeconds);
+        
+        if( isNaN(curMinutes) || isNaN(curSeconds) || isNaN(durMinutes) || isNaN(durSeconds) )
+            trackTime.removeClass('active');
+        else
+            trackTime.addClass('active');
+
+        
+    seekBar.width(playProgress+'%');
+    
+    if( playProgress == 100 )
+    {
+      i.attr('class','fa fa-play');
+      seekBar.width(0);
+            tProgress.text('00:00');
+            albumArt.removeClass('buffering').removeClass('active');
+            clearInterval(buffInterval);
+    }
+    }
+    
+    function checkBuffering()
+    {
+        clearInterval(buffInterval);
+        buffInterval = setInterval(function()
+        { 
+            if( (nTime == 0) || (bTime - nTime) > 1000  )
+                albumArt.addClass('buffering');
+            else
+                albumArt.removeClass('buffering');
+
+            bTime = new Date();
+            bTime = bTime.getTime();
+
+        },100);
+    }
+
+    function selectTrack(flag)
+    {
+        if( flag == 0 || flag == 1 )
+            ++currIndex;
+        else
+            --currIndex;
+
+        if( (currIndex > -1) && (currIndex < albumArtworks.length) )
+        {
+            if( flag == 0 )
+                i.attr('class','fa fa-play');
+            else
+            {
+                albumArt.removeClass('buffering');
+                i.attr('class','fa fa-pause');
+            }
+
+            seekBar.width(0);
+            trackTime.removeClass('active');
+            tProgress.text('00:00');
+            tTime.text('00:00');
+
+            currAlbum = albums[currIndex];
+            currTrackName = trackNames[currIndex];
+            currArtwork = albumArtworks[currIndex];
+
+            audio.src = trackUrl[currIndex];
+            
+            nTime = 0;
+            bTime = new Date();
+            bTime = bTime.getTime();
+
+            if(flag != 0)
+            {
+                audio.play();
+                playerTrack.addClass('active');
+                albumArt.addClass('active');
+            
+                clearInterval(buffInterval);
+                checkBuffering();
+            }
+
+            albumName.text(currAlbum);
+            trackName.text(currTrackName);
+            albumArt.find('img.active').removeClass('active');
+            $('#'+currArtwork).addClass('active');
+            
+            bgArtworkUrl = $('#'+currArtwork).attr('src');
+
+            bgArtwork.css({'background-image':'url('+bgArtworkUrl+')'});
+        }
+        else
+        {
+            if( flag == 0 || flag == 1 )
+                --currIndex;
+            else
+                ++currIndex;
+        }
+    }
+
+    function initPlayer()
+  { 
+        audio = new Audio();
+
+    selectTrack(0);
+    
+    audio.loop = false;
+    
+    playPauseButton.on('click',playPause);
+    
+    sArea.mousemove(function(event){ showHover(event); });
+    
+        sArea.mouseout(hideHover);
+        
+        sArea.on('click',playFromClickedPos);
+    
+        $(audio).on('timeupdate',updateCurrTime);
+
+        playPreviousTrackButton.on('click',function(){ selectTrack(-1);} );
+        playNextTrackButton.on('click',function(){ selectTrack(1);});
+  }
+    
+  initPlayer();
+});
+
